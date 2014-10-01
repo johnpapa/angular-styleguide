966518d4a70005ba528d720d9b93f9136a222891
Bahattin TOZYILMAZ <baha@udemy.com>
2014-09-30 18:13:37 -0700

    jshint

--- example/ng/apps/course-taking/app.js
+++ example/ng/apps/course-taking/app.js
@@ -1,9 +1,9 @@
 define(['angular', './discussion/discussion-module', './announcement/announcement-module'],
   function(angular) /*invoke*/ {
-    'use strict'
+    'use strict';
 
     angular.module('courseTakingApp', [
       'courseTakingApp.announcement',
       'courseTakingApp.discussion'
-    ])
+    ]);
   });
