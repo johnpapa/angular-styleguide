7eaa540db6e6fe09fa4f40cdd60522e01fb753a4
sergey <sergey@udemy.com>
2014-09-30 17:57:06 -0700

    added file-module conventions to example

--- example/ng/common/module.js
+++
@@ -1,9 +0,0 @@
-define(['angular', './common/comment/module', './common/course-learning/module'],
-  function(angular) /*invoke*/ {
-    'use strict'
-
-    angular.module('common', [
-      'common.comment',
-      'common.courseLearningGoal'
-    ])
-  });
