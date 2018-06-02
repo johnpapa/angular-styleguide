a27d00361d5ceff0c1040f588d7c1c874e32f82b
jamesliauw <jamesliauw@163.com>
2018-06-02 21:48:11 +0800

    feat: performance: {   hints: false }

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const path = require('path');
+const path = require("path");
 const pluginsConfig = require("./webpack.plguins.js");
 const rulesConfig = require("./webpack.rules.js");
 
 module.exports = {
 	entry: {
 		// 多入口文件
-		a: './src/js/index.ts',
-		b: './src/js/index2.ts',
-		jquery: 'jquery'
+		a: "./src/js/index.ts",
+		b: "./src/js/index2.ts",
+		jquery: "jquery",
 	},
 	output: {
-		path:path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, "dist"),
 		// 打包多出口文件
 		// 生成 a.bundle.js  b.bundle.js  jquery.bundle.js
-		filename: './js/[name].bundle.js'
+		filename: "./js/[name].bundle.js"
 	},
 	plugins: pluginsConfig,
 	devServer: {
@@ -21,16 +21,19 @@ module.exports = {
 		host: "localhost",
 		port: "8090",
 		open: true, // 开启浏览器
-		hot: false,   // 开启热更新
+		hot: false, // 开启热更新
 		inline: true,
 	},
-	devtool: "inline-source-map",  // 开启调试模式
-	module:{
-		rules: rulesConfig
+	devtool: "inline-source-map", // 开启调试模式
+	module: {
+		rules: rulesConfig,
 	},
 	resolve: {
-		extensions: [".tsx", ".ts", ".js",".json"]
+		extensions: [".tsx", ".ts", ".js", ".json"],
 	},
+	performance: {
+		hints: false
+	  },
 	// 提取js，lib1名字可改
 	optimization: {
 		splitChunks: {
@@ -38,10 +41,10 @@ module.exports = {
 				lib1: {
 					chunks: "initial",
 					name: "jquery",
-					enforce: true
-				}
-			}
-		}
-	}
+					enforce: true,
+				},
+			},
+		},
+	},
 
-}
+};
